960d7402381d5ebec7e70dc40bf4a34f5540840e
SpeedAddict <github@kaminski.cc>
2024-03-19 13:17:00 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,8 @@
 from machine import Pin
 from time import sleep
 
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-
-firmware_url = "https://raw.githubusercontent.com/speedaddict/esp32-blink-ota/main/"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "code.py")
-ota_updater.download_and_install_update_if_available()
-
 led = Pin(2, Pin.OUT)
 
 while True:
     led.value(not led.value())
-    sleep(.1)
+    sleep(1)
